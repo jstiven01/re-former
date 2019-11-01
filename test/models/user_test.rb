cfaf5c0096e7009046bbc2670404a4706563c11e
jstiven01 <stiven10@gmail.com>
2019-11-01 16:33:23 -0500

    Adding validations for presence to each field in the model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: 'damarisrpo', email: 'damarisrpo@gmail.com', password: '111111')
+  end
+
+  test 'Username should be present' do
+    @user.username = ''
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = ''
+    assert_not @user.valid?
+  end
+
+  test 'password should be present' do
+    @user.password = ''
+    assert_not @user.valid?
+  end
 end
